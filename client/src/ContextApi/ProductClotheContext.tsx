14958e66589c8dacf6c2c15247f223b409ba7f7a
Yazdani Chowdhury <yaz4dani@gmail.com>
2023-10-04 23:38:36 +0300

    WIP

--- client/src/ContextApi/ProductClotheContext.tsx
+++ client/src/ContextApi/ProductClotheContext.tsx
@@ -1,13 +1,6 @@
-import {
-  useState,
-  createContext,
-  useEffect,
-  FC,
-  ReactNode,
-  useContext,
-} from "react";
+import { useState, createContext, FC, ReactNode, useContext } from 'react';
 
-import { ProductClotheItem } from "../Dataprovider";
+import { ProductClotheItem } from '../Dataprovider';
 
 interface ProductClotheContextProps {
   addProduct: (newproductincart: ProductClotheItem) => void;
@@ -27,31 +20,27 @@ interface ProductClotheProviderProps {
   children: ReactNode;
 }
 
-export const ProductClotheProvider: FC<ProductClotheProviderProps> = ({
-  children,
-}) => {
-
+export const ProductClotheProvider: FC<ProductClotheProviderProps> = ({ children }) => {
   const [product, setProduct] = useState<ProductClotheItem[]>(() => {
-    const storedProduct = localStorage.getItem("cartProducts");
+    const storedProduct = localStorage.getItem('cartProducts');
     return storedProduct ? JSON.parse(storedProduct) : [];
   });
-  
+
   const addProduct = (newproduct: ProductClotheItem) => {
     const updatedProduct = [...product, newproduct];
     setProduct(updatedProduct);
-    localStorage.setItem("cartProducts", JSON.stringify(updatedProduct));
+    localStorage.setItem('cartProducts', JSON.stringify(updatedProduct));
   };
 
   const removeProduct = (productId: number) => {
     const updatedProduct = product.filter((item) => item.id !== productId);
     setProduct(updatedProduct);
-    localStorage.setItem("cartProducts", JSON.stringify(updatedProduct));
+    localStorage.setItem('cartProducts', JSON.stringify(updatedProduct));
   };
 
   // const addProduct = (newproduct: ProductClotheItem) => {
   //   setProduct([...product, newproduct]);
   // };
-
   // const removeProduct = (productId: number) => {
   //   const updatedProduct = product.filter((item) => item.id !== productId);
   //   setProduct(updatedProduct);
